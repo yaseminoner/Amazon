ce5d340af720d4f07d0304f38d9d6a55a536173c
yaseminoner <yasenermunchen@gmail.com>
2023-07-04 11:51:48 +0200

    runner class added

--- src/test/java/com/amazon/runner/CukesRunner.java
+++ src/test/java/com/amazon/runner/CukesRunner.java
@@ -1,4 +1,22 @@
 package com.amazon.runner;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+        },
+        features="src/test/resources/features",
+        glue="com/akdp/step_defs",
+        dryRun = false,
+        tags="@smoke",
+        publish = true
+)
 public class CukesRunner {
 }
--- src/test/java/com/amazon/runner/FailedTestRunner.java
+++ src/test/java/com/amazon/runner/FailedTestRunner.java
@@ -1,4 +1,16 @@
 package com.amazon.runner;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue = "com/akdp/step_defs",
+        features = "@target/rerun.txt"
+)
 public class FailedTestRunner {
+
+
 }
